37f26f02c6b46adda64ed882189de6462f25d741
ThatBaldDude4 <127453013+ThatBaldDude4@users.noreply.github.com>
2026-03-14 19:45:17 +0000

    add loading dom display

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ import { getData, renderCard  } from "./modules/data.js";
 
 const form = document.getElementById("weather-form");
 const locationInput = document.getElementById("location-input");
+const appContainer = document.getElementById("weather-app-container");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
     let input = locationInput.value;
     if (!input) {return};
-
+    showLoading();
     let data = await getData(input);
+    hideLoading();
     renderCard(data);
 });
+
+function showLoading() {
+    appContainer.textContent = "";
+    appContainer.textContent = "loading...";
+}
+
+function hideLoading() {
+    appContainer.textContent = "";
+}
